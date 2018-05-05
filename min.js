7e228bfa90316c9183a98a0143577118a47da3b8
Shyju <connectshyju@gmail.com>
2018-05-05 16:02:27 -0700

    Adding some more test files and updatng readme with results

--- min.js
+++ min.js
@@ -1,22 +1,31 @@
 var fs = require("fs");
 var espree = require("espree");
 
-var CompareMagic = function() {};
+var CompareMagic = function () {};
 
 var folderName = "testfiles";
 var resultsArray = [];
 
 var files = [
+  "angular-1.6.5.js",
+  "bootstrap-3.3.7.js",
+  "bootstrap-datepicker-1.8.0.js",
+  "chart-2.7.2.js",
+  "d3-4.13.0.js",
+  "inferno-5.0.4.js",
   "jquery-3.3.1.js",
   "jquery-3.3.1.slim.js",
   "jquery-ui-1.12.1.js",
   "loadash-4.17.10.js",
+  "moment-2.22.1.js",
   "mootools-1.6.0.js",
   "react-16.3.2.js",
+  "select2-4.0.5.js",
   "vue-2.5.16.js"
+
 ];
 
-CompareMagic.prototype.compareFiles = function(folder) {
+CompareMagic.prototype.compareFiles = function (folder) {
   resultsArray = [];
   files.forEach(file => {
     parseFileContent(file, folder);
@@ -37,13 +46,15 @@ function parseFileContent(fileName, folder) {
 
   resultsArray.push({
     file: fileName,
-    unminified : improvementIndex,
+    unminified: improvementIndex,
     minified: minifiedImprovementIndex
   });
 }
 
 function getImprovementIndex(fileName, code) {
-  const ast = espree.parse(code, { tokens: true });
+  const ast = espree.parse(code, {
+    tokens: true
+  });
   const contentLength = code.length;
   const tokenLength = ast.tokens.length;
 
